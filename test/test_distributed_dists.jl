1709d4fb8a5bcb87e81728ebc6fa2aebf59ee77d
tan <tanmaykm@gmail.com>
2015-12-11 10:12:58 +0530

    update tests

--- test/test_distributed_dists.jl
+++ test/test_distributed_dists.jl
@@ -6,14 +6,6 @@ using Base.Test
 macro test_colwise(dist, x, y, xd, yd, tol)
     quote
         local n = size($xd, 2)
-        r1 = zeros(n)
-        r2 = zeros(n)
-        r3 = zeros(n)
-        for j = 1 : n
-            r1[j] = evaluate($dist, ($x)[:,j], ($y)[:,j])
-            r2[j] = evaluate($dist, ($x)[:,1], ($y)[:,j])
-            r3[j] = evaluate($dist, ($x)[:,j], ($y)[:,1])
-        end
 
         tfx = @timed colwise($dist, $x, $y)
         tdx = @timed dcolwise($dist, $xd, $yd)
@@ -65,23 +57,23 @@ function colwise_test()
     Qd = distribute(Q; dist=(1,4));
 
     println("\tdistributed, singlenode")
-    @test_colwise SqEuclidean() X Y Xd Yd 1.0e-12
-    @test_colwise Euclidean() X Y Xd Yd 1.0e-12
-    @test_colwise Cityblock() X Y Xd Yd 1.0e-11
-    @test_colwise Chebyshev() X Y Xd Yd 1.0e-16
-    @test_colwise Minkowski(2.5) X Y Xd Yd 1.0e-13
-    @test_colwise Hamming() A B Ad Bd 1.0e-16
+    @test_colwise SqEuclidean() X Y Xd Yd eps()
+    @test_colwise Euclidean() X Y Xd Yd eps()
+    @test_colwise Cityblock() X Y Xd Yd eps()
+    @test_colwise Chebyshev() X Y Xd Yd eps()
+    @test_colwise Minkowski(2.5) X Y Xd Yd eps()
+    @test_colwise Hamming() A B Ad Bd eps()
 
-    @test_colwise CosineDist() X Y Xd Yd 1.0e-12
-    @test_colwise CorrDist() X Y Xd Yd 1.0e-12
+    @test_colwise CosineDist() X Y Xd Yd eps()
+    @test_colwise CorrDist() X Y Xd Yd eps()
 
-    @test_colwise ChiSqDist() X Y Xd Yd 1.0e-12
-    @test_colwise KLDivergence() P Q Pd Qd 1.0e-13
-    @test_colwise JSDivergence() P Q Pd Qd 1.0e-13
-    @test_colwise SpanNormDist() X Y Xd Yd 1.0e-12
+    @test_colwise ChiSqDist() X Y Xd Yd eps()
+    @test_colwise KLDivergence() P Q Pd Qd eps()
+    @test_colwise JSDivergence() P Q Pd Qd eps()
+    @test_colwise SpanNormDist() X Y Xd Yd eps()
 
-    @test_colwise BhattacharyyaDist() X Y Xd Yd 1.0e-12
-    @test_colwise HellingerDist() X Y Xd Yd 1.0e-12
+    @test_colwise BhattacharyyaDist() X Y Xd Yd eps()
+    @test_colwise HellingerDist() X Y Xd Yd eps()
     nothing
 end
 
@@ -111,36 +103,36 @@ function pairwise_test()
     Qd = distribute(Q; dist=(1,4));
 
     println("\tdistributed, singlenode")
-    @test_pairwise SqEuclidean() X Y Xd Yd 1.0e-11
-    @test_pairwise Euclidean() X Y Xd Yd 1.0e-11
-    @test_pairwise Cityblock() X Y Xd Yd 1.0e-11
-    @test_pairwise Chebyshev() X Y Xd Yd 1.0e-16
-    @test_pairwise Minkowski(2.5) X Y Xd Yd 1.0e-11
-    @test_pairwise Hamming() A B Ad Bd 1.0e-16
+    @test_pairwise SqEuclidean() X Y Xd Yd eps()
+    @test_pairwise Euclidean() X Y Xd Yd eps()
+    @test_pairwise Cityblock() X Y Xd Yd eps()
+    @test_pairwise Chebyshev() X Y Xd Yd eps()
+    @test_pairwise Minkowski(2.5) X Y Xd Yd eps()
+    @test_pairwise Hamming() A B Ad Bd eps()
 
-    @test_pairwise CosineDist() X Y Xd Yd 1.0e-11
-    @test_pairwise CorrDist() X Y Xd Yd 1.0e-11
+    @test_pairwise CosineDist() X Y Xd Yd eps()
+    @test_pairwise CorrDist() X Y Xd Yd eps()
 
-    @test_pairwise ChiSqDist() X Y Xd Yd 1.0e-11
-    @test_pairwise KLDivergence() P Q Pd Qd 1.0e-13
-    @test_pairwise JSDivergence() P Q Pd Qd 1.0e-13
+    @test_pairwise ChiSqDist() X Y Xd Yd eps()
+    @test_pairwise KLDivergence() P Q Pd Qd eps()
+    @test_pairwise JSDivergence() P Q Pd Qd eps()
 
-    @test_pairwise BhattacharyyaDist() X Y Xd Yd 1.0e-11
-    @test_pairwise HellingerDist() X Y Xd Yd 1.0e-11
+    @test_pairwise BhattacharyyaDist() X Y Xd Yd eps()
+    @test_pairwise HellingerDist() X Y Xd Yd eps()
 
     w = rand(m)
 
-    @test_pairwise WeightedSqEuclidean(w) X Y Xd Yd 1.0e-11
-    @test_pairwise WeightedEuclidean(w) X Y Xd Yd 1.0e-11
-    @test_pairwise WeightedCityblock(w) X Y Xd Yd 1.0e-11
-    @test_pairwise WeightedMinkowski(w, 2.5) X Y Xd Yd 1.0e-11
-    @test_pairwise WeightedHamming(w) A B Ad Bd 1.0e-11
+    @test_pairwise WeightedSqEuclidean(w) X Y Xd Yd eps()
+    @test_pairwise WeightedEuclidean(w) X Y Xd Yd eps()
+    @test_pairwise WeightedCityblock(w) X Y Xd Yd eps()
+    @test_pairwise WeightedMinkowski(w, 2.5) X Y Xd Yd eps()
+    @test_pairwise WeightedHamming(w) A B Ad Bd eps()
 
     Q = rand(m, m)
     Q = Q * Q'  # make sure Q is positive-definite
 
-    @test_pairwise SqMahalanobis(Q) X Y Xd Yd 1.0e-7
-    @test_pairwise Mahalanobis(Q) X Y Xd Yd 1.0e9
+    @test_pairwise SqMahalanobis(Q) X Y Xd Yd eps()
+    @test_pairwise Mahalanobis(Q) X Y Xd Yd eps()
     nothing
 end
 
